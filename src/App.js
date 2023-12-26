abc97e64b9e2e4ef0e2229a31db13e97b471e997
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-26 18:36:28 +0300

    modified routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../src/components/navbar/Navbar.jsx";
 import Products from "../src/pages/products/Products.jsx";
 import Homepage from "../src/pages/homepage/Homepage.jsx";
@@ -11,15 +11,15 @@ import ProductDetail from "./pages/products/productDetail/ProductDetail.jsx";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Homepage />}></Route>
           <Route exact path="/products" element={<Products />}></Route>
-          <Route exact path="/productDetail" element={<ProductDetail />}></Route>
+          <Route path="/productDetail/:id" element={<ProductDetail />} />
           <Route exact path="*" element={<div>Not Found</div>}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 };
