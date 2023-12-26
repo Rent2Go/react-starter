243d18dd61996b939e77b70c8c69b1803f3e954e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-26 14:37:30 +0300

    created routes & imported pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import Navbar from "../src/components/navbar/Navbar.jsx";
+import Products from "../src/pages/products/Products.jsx"
+import Homepage from "../src/pages/homepage/Homepage.jsx"
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
 
-export default App
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Homepage/>}></Route>
+          <Route exact path="/products" element={<Products/>}></Route>
+          <Route exact path="*" element={<div>Not Found</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
