cfb11469baaa70c45f14bdfd2dffe05cf04a41ca
Soner <syln.soner@gmail.com>
2023-12-27 03:55:45 +0300

    updated productcard

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -6,25 +6,21 @@ import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  // State tanımlamaları
   const [products, setProducts] = useState([]);
   const [idList, setIdList] = useState([0]);
   
 
-  // idList'e id ekleyen fonksiyon
   const addToIdList = (id) => {
     if (!idList.includes(id)) {
       setIdList([...idList, id]);
     }
   };
 
-  // Ürün silme işlemini başlatan fonksiyon
   const handleDelete = (id) => {
     addToIdList(id);
     alert('Silme işlemi gerçekleştiriliyor...');
   };
 
-  // Verilerin API'den alıp daha sonra  products state'ine atanması burada yapılıyor.
   useEffect(() => {
     axiosGet();
   }, []);
@@ -35,7 +31,6 @@ const ProductCard = ({ product }) => {
     console.log(response.data.products);
   };
 
-  // idList'teki elemanlar hariç yeni bir ürün listesi oluşturmak için yazdığım kısım 
   let newValue = products.filter((deleteProduct) => {
     return idList.map((id) => id !== deleteProduct.id).every(Boolean);
   });
@@ -51,14 +46,12 @@ const ProductCard = ({ product }) => {
               <h5 className="card-title">{product.title}</h5>
               <p className="card-text">{product.description}</p>
               <div className="action-row">
-                {/* Ürün detayına gitmek için buton */}
                 <Link
                   to={`/productDetail/${product.id}`}
                   className="btn btn-secondary btn-sm btn-review"
                 >
                   <SiCodereview /> Review
                 </Link>
-                {/* Ürünü silmek için buton */}
                 <Link
                   onClick={() =>  handleDelete(product.id)}
                   className="btn btn-danger btn-sm btn-review"
