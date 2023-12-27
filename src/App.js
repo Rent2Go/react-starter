1fe8d1aa6f7909f066381f7558c6cbc02f7899e3
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2023-12-27 04:05:14 +0300

    modified css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import ProductDetail from "./pages/products/productDetail/ProductDetail.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import AddProduct from "./pages/products/addProduct/AddProduct.jsx";
+import UpdateProduct from "./pages/products/updateProduct/UpdateProduct.jsx";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route exact path="/" element={<Homepage />}></Route>
           <Route exact path="/products" element={<Products />}></Route>
           <Route path="/productDetail/:id" element={<ProductDetail />} />
+          <Route path="/updateProduct/:id" element={<UpdateProduct />} />
           <Route exact path="/addProduct" element={<AddProduct />}></Route>
           <Route exact path="*" element={<div>Not Found</div>}></Route>
         </Routes>
--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -3,13 +3,14 @@ import "./productCard.css";
 import axios from "axios";
 import { SiCodereview } from "react-icons/si";
 import { FaTrash } from "react-icons/fa";
+import { RiEdit2Line } from "react-icons/ri";
+
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   // State tanımlamaları
   const [products, setProducts] = useState([]);
   const [idList, setIdList] = useState([0]);
-  
 
   // idList'e id ekleyen fonksiyon
   const addToIdList = (id) => {
@@ -21,7 +22,7 @@ const ProductCard = ({ product }) => {
   // Ürün silme işlemini başlatan fonksiyon
   const handleDelete = (id) => {
     addToIdList(id);
-    alert('Silme işlemi gerçekleştiriliyor...');
+    alert("Silme işlemi gerçekleştiriliyor...");
   };
 
   // Verilerin API'den alıp daha sonra  products state'ine atanması burada yapılıyor.
@@ -35,12 +36,11 @@ const ProductCard = ({ product }) => {
     console.log(response.data.products);
   };
 
-  // idList'teki elemanlar hariç yeni bir ürün listesi oluşturmak için yazdığım kısım 
+  // idList'teki elemanlar hariç yeni bir ürün listesi oluşturmak için yazdığım kısım
   let newValue = products.filter((deleteProduct) => {
     return idList.map((id) => id !== deleteProduct.id).every(Boolean);
   });
 
- 
   return (
     <div className="row">
       {newValue.map((product) => (
@@ -51,20 +51,26 @@ const ProductCard = ({ product }) => {
               <h5 className="card-title">{product.title}</h5>
               <p className="card-text">{product.description}</p>
               <div className="action-row">
-                {/* Ürün detayına gitmek için buton */}
                 <Link
                   to={`/productDetail/${product.id}`}
                   className="btn btn-secondary btn-sm btn-review"
                 >
-                  <SiCodereview /> Review
+                  <SiCodereview className="icon-review" /> Review
                 </Link>
-                {/* Ürünü silmek için buton */}
                 <Link
-                  onClick={() =>  handleDelete(product.id)}
+                  to={`/updateProduct/${product.id}`}
+                  className="btn btn-warning btn-sm btn-review"
+                >
+                  <RiEdit2Line />
+                  Update
+                </Link>
+                <Link
+                  onClick={() => handleDelete(product.id)}
                   className="btn btn-danger btn-sm btn-review"
                 >
                   <FaTrash /> Delete
                 </Link>
+              
               </div>
             </div>
           </div>
